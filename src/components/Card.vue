76dc6879479d5f7884a611d19925089ad7691e91
Olav Hartwedt Larsen <ohalar@gmail.com>
2020-12-12 14:11:08 +0100

    Changed element to read array from component to view to make it more general

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,11 @@
 <template>
-<div id="ocean-container">
+<div id="card-container">
   <v-card
-    v-for="ocean in oceans"
+    v-for="ocean in photos"
     :key="ocean.id"
     class="mx-auto"
     max-width="344"
-
+    id="card"
   >
     <v-img
       :src="`${ocean.src}`"
@@ -22,29 +22,21 @@
 
 <script>
   export default {
-      data() {
-          return {
-              oceans: [
-                  {id: 1, photographer: 'Emiliano Arano', src: 'https://images.pexels.com/photos/1295138/pexels-photo-1295138.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'},
-                  {id: 2, photographer: 'Jess Vide', src: 'https://images.pexels.com/photos/4321879/pexels-photo-4321879.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'},
-                  {id: 3, photographer: 'Pok Rie', src: 'https://images.pexels.com/photos/1655166/pexels-photo-1655166.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'},
-                  {id: 4, photographer: 'Kellie Churchman', src: 'https://images.pexels.com/photos/1001682/pexels-photo-1001682.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'},
-                  {id: 5, photographer: 'Matt Hardy', src:'https://images.pexels.com/photos/1533720/pexels-photo-1533720.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'},
-                  {id: 6, photographer: 'Josh Sorenson', src: 'https://images.pexels.com/photos/386148/pexels-photo-386148.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'},
-                  {id: 7, photographer: 'Emiliano Arano', src: 'https://images.pexels.com/photos/1330218/pexels-photo-1330218.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'},
-                  {id: 8, photographer: 'Hazy Mokhlas', src: 'https://images.pexels.com/photos/47480/pexels-photo-47480.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'}
-              ]
-          }
-      }
+      props: ['photos']
       
   }
 </script>
 
 <style lang="scss" scoped>
 
-#ocean-container {
+#card-container {
+    margin-top: 10vh;
     display: flex;
     flex-wrap: wrap;
+
+    #card {
+        margin-bottom: 5vh;
+    }
 }
 
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <card />
+  <div class="oceans">
+    <card v-bind:photos="oceans"/>
 
     
   </div>
@@ -13,6 +12,20 @@ import Card from '@/components/Card.vue'
 
 export default {
   name: 'About',
+  data() {
+          return {
+              oceans: [
+                  {id: 1, photographer: 'Emiliano Arano', src: 'https://images.pexels.com/photos/1295138/pexels-photo-1295138.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'},
+                  {id: 2, photographer: 'Jess Vide', src: 'https://images.pexels.com/photos/4321879/pexels-photo-4321879.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'},
+                  {id: 3, photographer: 'Pok Rie', src: 'https://images.pexels.com/photos/1655166/pexels-photo-1655166.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'},
+                  {id: 4, photographer: 'Kellie Churchman', src: 'https://images.pexels.com/photos/1001682/pexels-photo-1001682.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'},
+                  {id: 5, photographer: 'Matt Hardy', src:'https://images.pexels.com/photos/1533720/pexels-photo-1533720.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'},
+                  {id: 6, photographer: 'Josh Sorenson', src: 'https://images.pexels.com/photos/386148/pexels-photo-386148.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'},
+                  {id: 7, photographer: 'Emiliano Arano', src: 'https://images.pexels.com/photos/1330218/pexels-photo-1330218.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'},
+                  {id: 8, photographer: 'Hazy Mokhlas', src: 'https://images.pexels.com/photos/47480/pexels-photo-47480.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'}
+              ]
+          }
+      },
   components: {
     Card
   }
