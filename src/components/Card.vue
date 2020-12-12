f43a94ca8c4f30a1c186c83343f1a7df3b5cf941
Olav Hartwedt Larsen <ohalar@gmail.com>
2020-12-12 13:15:38 +0100

    Added simple card to about/ocean-page

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-img
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      height="200px"
+    ></v-img>
+
+    <v-card-title>
+      Top western road trips
+    </v-card-title>
+
+  </v-card>
+</template>
+
+<script>
+  export default {
+  }
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <card />
+
+    
   </div>
 </template>
+
+<script>
+
+import Card from '@/components/Card.vue'
+
+export default {
+  name: 'About',
+  components: {
+    Card
+  }
+}
+</script>
